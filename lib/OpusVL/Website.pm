402ac4d365ea608b9694d5d390e7f858bd93b586
Colin Newell <colin@opusvl.com>
2015-04-23 15:06:42 +0100

    Added pod

--- lib/OpusVL/Website.pm
+++ lib/OpusVL/Website.pm
@@ -14,3 +14,22 @@ my $builder = OpusVL::Website::Builder->new(
 $builder->bootstrap;
 
 1;
+
+=head1 NAME
+
+OpusVL::Website - Vanilla CMS Frontend website
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 ATTRIBUTES
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 OpusVL.
+
+This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+
+=cut
