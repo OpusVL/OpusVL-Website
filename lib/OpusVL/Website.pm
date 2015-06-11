7b60870564e84882f09e72cdb86fc7318ed27e0b
Colin Newell <colin@opusvl.com>
2015-06-11 09:53:39 +0100

    Bumped version number

--- lib/OpusVL/Website.pm
+++ lib/OpusVL/Website.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use OpusVL::Website::Builder;
 
-our $VERSION = "0.07";
+our $VERSION = "0.08";
 
 my $builder = OpusVL::Website::Builder->new(
     appname => __PACKAGE__,
